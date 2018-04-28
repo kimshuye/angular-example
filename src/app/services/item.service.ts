90a5ce93848d4f912e74d1f61dbf4b4e3d3ec7fd
kimshuye <suntisuk.go@gmail.com>
2018-04-28 13:04:04 +0700

    update service item

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,11 +1,43 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
 @Injectable()
 export class ItemService {
 
-  items;
+  items:AngularFireList<Item>;
+  userId:string;
 
-  constructor() { }
+  constructor(
+    private db:AngularFireDatabase,
+    private afAuth:AngularFireAuth    
+  ) { 
+    this.afAuth.authState.subscribe(user => {
+      if(user) this.userId = user.uid;
+    });
+  }
+
+  getItemList(): AngularFireList<Item>{
+    if(!this.userId) return;
+    this.items = this.db.list(`items/${this.userId}`);
+    return this.items;
+  }
+
+  createItem(item:Item){
+    this.items.push(item);
+  }
+
+  join(itemKey){
+    const data = { [this.userId]:true};
+    const members = this.db.object(`items/${itemKey}/members`);
+    members.update(data);
+  }
+
+  leave(itemKey){
+    const member = this.db.object(`items/${itemKey}/members/${this.userId}`);
+    member.remove();
+  }
 
 }
 
